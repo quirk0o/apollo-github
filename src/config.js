dbf80099578761fcd59beaad9b8c2d0ad5acb47f
Beata Obrok <beata.obrok@u2i.com>
2022-04-02 20:51:32 +0200

    Initialize project using Create React App

---
+++ src/config.js
@@ -0,0 +1,11 @@
+function getConfigOption(name, defaultValue = undefined) {
+  const value = rocess.env[`REACT_APP_${name}`];
+
+  if (value !== undefined) {
+    return value;
+  } else if (defaultValue !== undefined && typeof defaultValue === "string") {
+    return defaultValue;
+  } else {
+    throw new Error(`Missing configuration REACT_APP_${name}`);
+  }
+}
